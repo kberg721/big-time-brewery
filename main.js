a499bc0f83d2676185d0dff62c5b547712009a50
Kyle Bergman <Kberg721@Kyles-MacBook-Pro.local>
2014-11-30 22:43:14 -0800

    added preliminary website

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+ 
+  $("#carousel").owlCarousel({
+  	  navigation : false, // Show next and prev buttons
+      slideSpeed : 500,
+      paginationSpeed : 400,
+      singleItem:true,
+      autoPlay:true
+  });
+ 
+});
